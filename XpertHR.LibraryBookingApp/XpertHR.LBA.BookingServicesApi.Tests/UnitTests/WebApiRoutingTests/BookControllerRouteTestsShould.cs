dc62554dda4005212a6248d1efcc9dbf07bda1b2
thanhdoan <thanh.doan1@rbi.co.uk>
2017-07-28 13:53:11 +0100

    Init test for routing

---
+++ XpertHR.LibraryBookingApp/XpertHR.LBA.BookingServicesApi.Tests/UnitTests/WebApiRoutingTests/BookControllerRouteTestsShould.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using MvcRouteTester;
+using NUnit.Framework;
+using XpertHR.LBA.BookingServicesApi.Controllers;
+
+namespace XpertHR.LBA.BookingServicesApi.Tests.UnitTests.WebApiRoutingTests
+{
+    [TestFixture]
+    public class BookControllerRouteTestsShould
+    {
+        private HttpConfiguration _httpConfiguration;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _httpConfiguration = new HttpConfiguration();
+            WebApiConfig.Register(_httpConfiguration);
+            _httpConfiguration.EnsureInitialized();
+        }
+
+        [Test]
+        public void BeAbleGetAllBookWithCorrectRouteCallAppropirateMethod()
+        {
+            const string route = "/api/books/getall";
+            RouteAssert.HasApiRoute(_httpConfiguration, route, HttpMethod.Get);
+            _httpConfiguration.ShouldMap(route).To<BookController>(HttpMethod.Get, x => x.GetAll());
+        }
+    }
+}
---
+++ XpertHR.LibraryBookingApp/XpertHR.LBA.BookingServicesApi/Controllers/BookController.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace XpertHR.LBA.BookingServicesApi.Controllers
+{
+    [RoutePrefix("api/books")]
+    public class BookController : ApiController
+    {
+        [Route("getall")]
+        [HttpGet]
+        public HttpResponseMessage GetAll()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
